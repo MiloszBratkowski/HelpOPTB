8d67db4059c5c1b5fbe17e8e0809ebca6f52f49b
MiloszBratkowski <76225054+MiloszBratkowski@users.noreply.github.com>
2023-02-16 21:58:27 +0100

    Base commit

---
+++ src/main/java/pl/techbrat/spigot/helpop/HelpOPTB.java
@@ -0,0 +1,17 @@
+package pl.techbrat.spigot.helpop;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class HelpOPTB extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}

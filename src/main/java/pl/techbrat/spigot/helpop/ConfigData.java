c24f18b071f16e75baaab9144b6c3bd3c31db950
MiloszBratkowski <76225054+MiloszBratkowski@users.noreply.github.com>
2023-03-19 22:05:57 +0100

    Fast fix autogenerate config and messages.

--- src/main/java/pl/techbrat/spigot/helpop/ConfigData.java
+++ src/main/java/pl/techbrat/spigot/helpop/ConfigData.java
@@ -31,7 +31,7 @@ public class ConfigData {
     public ConfigData() {
         instance = this;
 
-        createConfigs(true);
+        createConfigs(false);
 
         plugin.getLogger().log(Level.INFO, "Loading config file...");
 
